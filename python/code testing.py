b52948544b85c79e3399413d58134813357f33c9
da <davidalton1@gmail.com>
2025-02-27 15:57:46 -0600

    some examples

---
+++ python/code testing.py
@@ -0,0 +1,46 @@
+# various code tests and examples
+# code that requirese input are commented out to keep it running
+
+
+# define string, print it
+str='project arcturus v1'
+print(str)
+
+
+# print text
+print ("project arcturus v2")
+
+
+# assign value to variable
+# data type set to integer
+
+
+# assign a boolean variable, print type
+a= True
+#  a is a boolean type
+print(type(a))
+
+
+# show lists are mutable
+a = [12, 28.5, "project"]
+# print list index element
+print(a[1])
+# lists are mutable
+a[1]= 30
+print(a)
+
+
+#  define dictionary, print entry
+D = {'a': "AI", 'c': "Cloud", 'w': "Arcturus"}
+print(D['c'])
+
+
+# define two variables, print msg w/variables
+user = "Hank"
+lines = "datatypes"
+# print("yay, " + user + "! Your learned " + str(lines) + " in python")
+
+#  print() w/output text/variable
+name = "Hank Scorpio"
+age = 23
+print("Hello, my name is", name, "and I am", age, "years old.")
